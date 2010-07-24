470ffa9e3f8c58e0c149a145965c07ed05dcda23
dirk <dirk@fritz.(none)>
2010-07-24 11:59:31 +0200

    autoconf version bump

--- src/proc_impl.c
+++ src/proc_impl.c
@@ -10,63 +10,65 @@ const char proc_dump_format[]="%6d %8llu %s\n";
 static
 int cmppid(const void* l, const void* r)
 {
-  return *((pid_t*)l)-*((pid_t*)r);
+   return *((pid_t*)l)-*((pid_t*)r);
 }
 
 static
 void cleanup_proc(proc_t* p)
 {
-  if (p->cmdline)
-    free((void*)*p->cmdline);
+   if (p->cmdline)
+      free((void*)*p->cmdline);
 }
 
-#define SWAP(Type,parg1,parg2) \
-{ \
-  Type *p1=parg1, *p2=parg2, tmp=*p1; \
-  *p1=*p2; \
-  *p2=tmp; \
-}
+#define SWAP(Type,parg1,parg2)                  \
+   do                                           \
+   {                                            \
+      Type *p1=parg1, *p2=parg2, tmp=*p1;       \
+      *p1=*p2;                                  \
+      *p2=tmp;                                  \
+   }                                            \
+   while(0)
 
 static
 int init_check_procs(size_t nProcs, pid_t* pids, unsigned long long* startTimes,
 		     bool verbose)
 {
-  PROCTAB* ptp=openproc(PROC_FILLARG|PROC_FILLSTAT);
-  if(!ptp)
-    {
+   PROCTAB* ptp=openproc(PROC_FILLARG|PROC_FILLSTAT);
+   if(!ptp)
+   {
       fprintf(stderr, "Error: cannot access /proc.\n");
       exit(-1);
-    }
+   }
 
-  if(verbose)
-    {
+   if(verbose)
+   {
       printf("monitoring:\n");
       printf(proc_dump_header);
-    }
+   }
 
-  pid_t* pidsstart=pids;
-  proc_t buf;
-  while(readproc(ptp,&buf))
-    {
+   pid_t* pidsstart=pids;
+   proc_t buf;
+   while(readproc(ptp,&buf))
+   {
       pid_t* p=
-	(pid_t*)lfind(&buf.tgid,pids,&nProcs,sizeof(pid_t),cmppid);
+         (pid_t*)lfind(&buf.tgid,pids,&nProcs,sizeof(pid_t),cmppid);
       if(p!=NULL)
-	{
-	  SWAP(pid_t,p,pids);
+      {
+         SWAP(pid_t,p,pids);
 
-	  *startTimes=buf.start_time;
-	  --nProcs;++pids;++startTimes;
+         *startTimes=buf.start_time;
+         --nProcs;++pids;++startTimes;
 
-	  if(verbose)
-	      printf(proc_dump_format,buf.tgid,buf.start_time,buf.cmd);
-	}
+         if(verbose)
+            printf(proc_dump_format,buf.tgid,buf.start_time,buf.cmd);
+      }
       cleanup_proc(&buf);
-    }
-  if(verbose)
-    printf("-----\n");
+   }
+   if(verbose)
+      printf("-----\n");
 
-  closeproc(ptp);
-  return pids-pidsstart; // processes_found
+   closeproc(ptp);
+   return pids-pidsstart; // processes_found
 }
 
 /* return-value:  */
@@ -77,82 +79,82 @@ static
 int check_procs(size_t nProcs,pid_t* pids, unsigned long long* startTimes,
 		size_t treshold, bool verbose)
 {
-  PROCTAB* ptp=openproc(PROC_FILLSTAT);
-  if(!ptp)
-    {
+   PROCTAB* ptp=openproc(PROC_FILLSTAT);
+   if(!ptp)
+   {
       fprintf(stderr, "Error: cannot access /proc.\n");
       exit(-2);
-    }
+   }
 
-  pid_t* pidsstart=pids;
-  proc_t buf;
-  while(nProcs && readproc(ptp,&buf))
-    {
+   pid_t* pidsstart=pids;
+   proc_t buf;
+   while(nProcs && readproc(ptp,&buf))
+   {
       pid_t* p=
-	(pid_t*)lfind(&buf.tgid,pids,&nProcs,sizeof(pid_t),cmppid);
+         (pid_t*)lfind(&buf.tgid,pids,&nProcs,sizeof(pid_t),cmppid);
       if(p!=NULL)
-	{
-	  --nProcs;
-	  if(startTimes[p-pids]==buf.start_time)
-	    {
-	      /* pids are stored in the order that they are found;
-		 we assume that the next run will returns results in the same order
-		 this should speed up our linear array lookup */
-	      SWAP(pid_t,p,pids);
-	      SWAP(unsigned long long,startTimes,startTimes+(p-pids));
-	      /* an entry can only be found once, so increment base value */
-	      ++pids,++startTimes;
-	      if((pids-pidsstart)>treshold)
-		{
-		  pids+=nProcs;
-		  /* return overestimate (pretend all remaining are matches) */
-		  cleanup_proc(&buf);
-		  break;
-		}
-	    }
-	  else
-	    {
-	      if(verbose)
-		{
-		  printf("reused process id detected: %d\n",buf.tgid);
-		  printf(proc_dump_format,buf.tgid,buf.start_time,buf.cmd);
-		}
-	      /* ignore restarted pids in future runs */
-	      *p=pids[nProcs];
-	      startTimes[p-pids]=startTimes[nProcs];
-	    }
-	}
+      {
+         --nProcs;
+         if(startTimes[p-pids]==buf.start_time)
+         {
+            /* pids are stored in the order that they are found;
+               we assume that the next run will returns results in the same order
+               this should speed up our linear array lookup */
+            SWAP(pid_t,p,pids);
+            SWAP(unsigned long long,startTimes,startTimes+(p-pids));
+            /* an entry can only be found once, so increment base value */
+            ++pids,++startTimes;
+            if((pids-pidsstart)>treshold)
+            {
+               pids+=nProcs;
+               /* return overestimate (pretend all remaining pids are matches) */
+               cleanup_proc(&buf);
+               break;
+            }
+         }
+         else
+         {
+            if(verbose)
+            {
+               printf("reused process id detected: %d\n",buf.tgid);
+               printf(proc_dump_format,buf.tgid,buf.start_time,buf.cmd);
+            }
+            /* ignore restarted pids in future runs */
+            *p=pids[nProcs];
+            startTimes[p-pids]=startTimes[nProcs];
+         }
+      }
       cleanup_proc(&buf);
-    }
-  closeproc(ptp);
-  return pids-pidsstart;
+   }
+   closeproc(ptp);
+   return pids-pidsstart;
 }
 
 int
 proc_observe_processes(size_t nProcsInit,pid_t* pids,size_t running,bool batch,
 		       bool verbose, int nInterval)
 {
-  unsigned long long startTimes[nProcsInit];
-  for(int i=0;i<nProcsInit;++i)
-    {
+   unsigned long long startTimes[nProcsInit];
+   for(int i=0;i<nProcsInit;++i)
+   {
       startTimes[i]=~0ULL;
-    }
+   }
 
-  size_t nProcs=init_check_procs(nProcsInit,pids,startTimes,verbose);
-  if(!batch && !nProcs)
-    {
+   size_t nProcs=init_check_procs(nProcsInit,pids,startTimes,verbose);
+   if(!batch && !nProcs)
+   {
       if(verbose)
-	fprintf(stderr, "no processes found, bailing out\n");
+         fprintf(stderr, "no processes found, bailing out\n");
       return -4;
-    }
+   }
 
-  while(1)
-    {
+   while(1)
+   {
       nProcs=check_procs(nProcs,pids,startTimes,running,verbose);
       if(nProcs<=running)
-	break;
+         break;
       sleep(nInterval); // TODO: maybe time has to include time taken for check_procs
-    }
+   }
 
-  return 0;
+   return 0;
 }
